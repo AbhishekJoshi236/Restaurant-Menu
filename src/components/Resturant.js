a4185e16ea22ff2a6453a01230efa0543d7b7d08
Abhishek Joshi <allabhishekjoshi.com>
2023-08-24 14:38:58 +0530

    first commit

---
+++ src/components/Resturant.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import "./style.css";
+import Menu from "./menuApi.js";
+import MenuCard from "./MenuCard";
+import Navbar from './Navbar';
+
+const uniqueList=[
+    ... new Set(
+        Menu.map((currElem)=>{
+            return currElem.category;
+        })
+    ),
+    "All",
+];
+
+
+const Resturant = () => {
+
+    const[menuData, setMenuData] = useState(Menu);
+    // const[menuList, setmenuList] = useState(uniqueList);
+
+    const filterItem = (category)=>{
+        if(category == "All"){
+    
+            setMenuData(Menu);
+            return;
+        }
+        const updatedList = Menu.filter((currEle) =>{
+            return currEle.category === category;
+        });
+        setMenuData(updatedList);
+    };
+    
+    return (
+        <>
+        <Navbar filterItem={filterItem} menuList={uniqueList}/>
+        <MenuCard menuData={menuData} />
+        </>
+    )
+}
+
+export default Resturant;
